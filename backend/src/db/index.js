a21cf15d027ec6734570255e25136431b6346a41
devYashraj <capyashraj007@gmail.com>
2025-02-03 17:54:02 +0530

    add recursive connectDB function

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -4,11 +4,9 @@ import { DB_NAME } from '../constants.js'
 async function connectDB() {
     try {
         await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`);
-        console.log("Connected to DB successfully");
     }
     catch (error) {
-        console.log("DB connection failed");
-        console.log(error);
+        throw error;
     }
 }
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -6,13 +6,23 @@ dotenv.config({
     path: "./.env"
 });
 
+const startServer = async (retryCount=1) => {
+    try {
+        await connectDB();
+        console.log(`Connected to DB successfully!`);
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server listening on PORT ${PORT}`);
+        })
+    } 
+    catch (error) {
+        console.log(error);
+        console.log(`DB connection failed. Attempt ${retryCount}`);
+        console.log(`Retrying in 5 seconds...`);
+        setTimeout(()=>{
+            startServer(retryCount+1);
+        }, 5000)
+    }
+}
 
-connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server listening on PORT ${process.env.PORT}`);
-    })
-})
-.catch((error) => {
-    console.log("DB connection failed ",error);
-})
+startServer();
