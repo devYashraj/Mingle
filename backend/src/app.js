4da8f0e1663b4a74bee589908d03cebee826182d
devYashraj <capyashraj007@gmail.com>
2025-02-10 17:35:22 +0530

    add nginx load balancer

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,6 +27,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.enable('trust proxy');
 
 import userRouter from './routes/user.routes.js'
 import followRouter from './routes/following.routes.js';
